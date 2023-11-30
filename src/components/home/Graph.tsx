489a49e3c2cae3e29b22c2acc4a5ad27ccbf88c3
vvu060 <vvu060@gmail.com>
2023-11-30 19:30:45 +0530

    create graph component

---
+++ src/components/home/Graph.tsx
@@ -0,0 +1,23 @@
+import Button from '../shared/Button';
+import ChooseTailcall from './ChooseTailcall';
+
+const Graph = () => {
+  return (
+    <section className='bg-tailCall-dark-700 h-full w-full text-tailCall-light-100'>
+      <div className='max-w-7xl mx-auto my-20 py-20'>
+        <div className='flex items-center justify-between'>
+          <h5 className='text-display-medium max-w-lg'>
+            Platform made for performance.
+          </h5>
+          <Button title='View on Github' onClick={() => {}} theme='dark' />
+        </div>
+
+        {/* Graphs */}
+
+        <ChooseTailcall />
+      </div>
+    </section>
+  );
+};
+
+export default Graph;
