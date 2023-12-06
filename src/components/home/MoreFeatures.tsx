ed3fdd34c0e67000b53bdf29c22d63f4e228d195
vvu060 <vvu060@gmail.com>
2023-12-06 10:25:27 +0530

    make more features responsive

--- src/components/home/MoreFeatures.tsx
+++ src/components/home/MoreFeatures.tsx
@@ -7,9 +7,9 @@ const FeatureBox = ({ feature }) => {
       style={{
         borderBottom: '1px solid #E4E4E4',
       }}
-      className='flex items-center space-x-4 text-title-medium text-tailCall-dark-500 h-32 w-full border border-tailCall-dark-300'
+      className='flex flex-col sm:flex-row items-start justify-start sm:items-center text-start my-2 space-y-2 pb-2 sm:space-x-4 text-content-tiny font-bold sm:text-title-medium text-tailCall-dark-500  sm:h-32 w-full border border-tailCall-dark-300'
     >
-      <feature.logo className='w-8 h-8' />
+      <feature.logo className='w-6 h-6 sm:w-8 sm:h-8' />
       <span>{feature.title}</span>
     </div>
   );
@@ -18,11 +18,11 @@ const FeatureBox = ({ feature }) => {
 const MoreFeatures = () => {
   return (
     <section className='w-full mb-20'>
-      <div className='max-w-7xl mx-auto text-center'>
-        <h5 className='text-display-small text-tailCall-dark-500'>
+      <div className='max-w-7xl mx-4 sm:mx-auto text-center'>
+        <h5 className='text-title-semi-large sm:text-display-small text-tailCall-dark-500'>
           There’s so much more.
         </h5>
-        <div className='grid grid-cols-2 my-16 gap-x-10'>
+        <div className='grid grid-cols-2 my-4 sm:my-16 gap-x-6 sm:gap-x-10'>
           {moreFeatures.map((feature) => (
             <FeatureBox feature={feature} key={feature.id} />
           ))}
