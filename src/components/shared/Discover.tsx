376bffb0960e812fb9b15de088fd97edd341fb0f
vvu060 <vvu060@gmail.com>
2023-12-06 11:17:49 +0530

    make discover section responsive

--- src/components/shared/Discover.tsx
+++ src/components/shared/Discover.tsx
@@ -4,17 +4,17 @@ import Button from './Button';
 const Discover = () => {
   return (
     <section>
-      <div className='bg-tailCall-yellow relative flex items-center justify-center'>
-        <div className='h-full'>
+      <div className='bg-tailCall-yellow relative flex items-center justify-center h-[208px] sm:h-full'>
+        <div className='sm:h-full text-center mt-20 sm:mt-0'>
           <BgTailcall className='w-[90%]' />
         </div>
 
-        <div className='flex flex-col items-center absolute max-w-2xl space-y-6'>
-          <h5 className='text-display-medium text-center'>
+        <div className='flex flex-col items-center absolute max-w-2xl space-y-4 sm:space-y-6'>
+          <h5 className='text-title-semi-large sm:text-display-medium text-center mb-0'>
             Discover the power of enterprise solution.
           </h5>
 
-          <div className='flex space-x-6'>
+          <div className='flex space-x-3 sm:space-x-6'>
             <Button theme='dark' title='Get in touch' onClick={() => {}} />
             <Button theme='light' title='Know More' onClick={() => {}} />
           </div>
